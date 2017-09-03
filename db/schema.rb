e2884c5b5fbff6e1a4d5e2cf7aa75b2124b0b123
Catharina Mesquita <catha.ana.1994@gmail.com>
2017-09-03 16:44:27 -0300

    change database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170831001642) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ambulaces", force: :cascade do |t|
     t.string "type"
     t.boolean "availability"
